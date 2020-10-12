6dfedc6db646c24f07dfb7a9f02ccb0e5bc42cc8
TheHCL <daniel8411222@gmail.com>
2020-10-12 03:52:01 -0500

    Modified code and readme

--- gta_auto_survival_afk53k.py
+++ gta_auto_survival_afk53k.py
@@ -5,13 +5,15 @@ import os
 import pyautogui as py 
 import ctypes as c
 
+flag =0
+
 def d_click():                  #direct input for mouse
     c.windll.user32.mouse_event(2, 0,0,0,0)
     time.sleep(0.01)
     c.windll.user32.mouse_event(4, 0,0,0,0)
 
 
-def job():  # idle auto with direct input
+def job():  # no use
     p.keyDown('w')
     time.sleep(0.5)
     p.keyUp('w')
@@ -20,43 +22,76 @@ def job():  # idle auto with direct input
     p.keyUp('s')
     time.sleep(5)
 
-def test():   # where i use to test my script
-    p.keyDown('w')
-    time.sleep(0.5)
-    p.keyUp('w')
-    p.keyDown('s')
-    time.sleep(0.5)
-    p.keyUp('s')
-    time.sleep(5)
+def playtab():   
+    play2 = py.locateOnScreen("gta_pic/play2.PNG",confidence = 0.8,grayscale =True)
+    global flag
+    if play2 is None:
+        p.press("s")
+        time.sleep(0.1)
+
+        
+    else:  
+        p.keyDown('enter')
+        time.sleep(0.02)
+        p.keyUp('enter')
+        flag = 1
+        time.sleep(5)
     
+def replaytab():
+    replay1 = py.locateCenterOnScreen("gta_pic/replay1.PNG",confidence = 0.7,grayscale=True)
+    global flag
 
-def check():  # to check if the mission is complete and replay after complete
-    play = py.locateCenterOnScreen("gta_pic/play.PNG",confidence = 0.9)
-    time.sleep(1)
-    replay = py.locateCenterOnScreen("gta_pic/replay.PNG",confidence = 0.9)
-    time.sleep(1)
-    if play is not None:
-        x ,y = play
-        py.moveTo(x=x+300,y = y)
-        py.moveTo(play)
-        time.sleep(1)
-        d_click()
+    if replay1 is None:
+        p.press("s")
+        time.sleep(0.1)
+        
+    else:  
+        p.keyDown('enter')
+        time.sleep(0.02)
+        p.keyUp('enter')
+        time.sleep(5)
+        flag = 2
+        
+def play2tab():
+    play3 = py.locateCenterOnScreen("gta_pic/play3.PNG",confidence = 0.9,grayscale=True)
+    global flag
+
+    if play3 is None:
+        p.press("s")
+        time.sleep(0.1)
+        
+    else:  
+        p.keyDown('enter')
+        time.sleep(0.02)
+        p.keyUp('enter')
+        time.sleep(5)
+        flag = 0        
+
+
     
         
     
-    if replay is not None:
-        x ,y = replay
-        py.moveTo(x=x-300,y = y)
-        py.moveTo(replay)
-        time.sleep(1)
-        d_click()
-    job()
+
+def check():  # add flag to determine which script to run
+    global flag
+    if flag == 0:
+        playtab()
+        print("Set up")
+    if flag == 1:
+        replaytab()
+        print("In Game")
+    
+    if flag == 2:
+        play2tab()
+        print("set up 2")
 
 
 # i am the main function===============================
+
 time.sleep(10)
 while 1:
     check()
+    
     
     if k.is_pressed('*'):
         break
